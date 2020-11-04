61af3b00428d81bf49edb5ac7c65ba8049f84922
Rigoberto Perez Ovando <rperez@cobach.edu.mx>
2020-11-04 17:04:21 -0600

    estructura inicial
    se suben la estructura básica del sitio

---
+++ Nosotros/nosotros.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css" />
+    <title>zapaterias.com</title>
+</head>
+
+    <body >
+	<!-- Header -->
+	<header id="header" >
+	<h1><strong><a href="index.php">Zapaterias.com</a></strong></h1>
+	<nav id="nav">
+		<ul>
+			<li><a href="../index.php">Inicio</a></li>
+            <li><a href="../Catalogo/catalogo.php">Catálogo</a></li>
+            <li><a href="../Compras/compras.php">Mis Compras</a></li>
+            <li><a href="../Nosotros/nosotros.php">Nosotros</a></li>
+            <li><a href="../Administracion/administrar.php">Administración</a></li>
+            <li><a href="contactanos">Contáctanos</a></li>			
+		</ul>
+	</nav>
+	</header>
+	<!-- Main -->
+    <section id="main" class="wrapper">
+		<div class="container">
+
+			<header class="major special">
+				<h2>Nosotros</h2>
+				<p>...</p>
+			</header>
+
+		</div>
+		</section>
+	<!-- Footer -->
+	<footer id="footer">
+		<div class="container">
+			<ul class="icons">
+				<li><a href="#" class="icon fa-facebook"></a></li>
+				<li><a href="#" class="icon fa-twitter"></a></li>
+				<li><a href="#" class="icon fa-instagram"></a></li>
+			</ul>
+			<ul class="copyright">
+				<li>&copy; UNACH</li>
+				<li>Design: <a href="#">LSC</a></li>
+						
+			</ul>
+		</div>
+	</footer>
+
+
+	</body>
+</body>
+</html>
